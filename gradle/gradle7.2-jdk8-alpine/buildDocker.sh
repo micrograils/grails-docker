68ccc5a6f5b80512fe00c37a490f4ff2c08f8f88
MicroGrails <aaron@micrograils.com>
2022-08-20 21:11:23 +0800

    Add Dockerfile for gradle7.2-jdk8-alpine.

--- gradle/gradle7.2-jdk8-alpine/buildDocker.sh
+++ gradle/gradle7.2-jdk8-alpine/buildDocker.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-version=gradle7.2-jdk8-alpine
+version=7.2-jdk8-alpine
 
 mkdir build && copy Dockerfile build/ && cd build
 
 sudo docker build -t micrograils/gradle:$version .
+
+rm -rf build/
